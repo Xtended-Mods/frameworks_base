40895ecf508e4a9ad2214274e2f3b168cce4377a
SpiritCroc <dev@spiritcroc.de>
2018-11-07 15:02:52 +0100

    Add back more battery bar locations [1/2]
    **SDB- Add option for Below QS Header (For Notch Devices)
    
    Based on orignal battery bar commit by
    cphelps76 <cphelps76@droidconcepts.com>
    
    - Bring to Pie
    - Add bottom of statusbar location
    - Use extra preference for location so we can use our masterswitch
        preference in AE for en-/disabling
    - Leave away rotated navigation bar for now
        depending whether rotated to left or right, battery bar would show
        up on other edge, so would need extra logic
    
    Change-Id: I9b323e025d04a3a16b5557f96f0cd5ad08dce0dc
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -29,6 +29,7 @@ import android.annotation.Nullable;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.database.ContentObserver;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.os.UserHandle;
@@ -134,6 +135,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private final Executor mMainExecutor;
 
     private int mClockStyle;
+
+    private View mBatteryBars[] = new View[2];
 
     private BatteryMeterView mBatteryMeterView;
     private StatusIconContainer mStatusIcons;
@@ -289,6 +292,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mStatusBarIconController.addIconGroup(mDarkIconManager);
         mSystemIconArea = mStatusBar.findViewById(R.id.system_icon_area);
         mClockView = mStatusBar.findViewById(R.id.clock);
+        mBatteryBars[0] = mStatusBar.findViewById(R.id.battery_bar);
+        mBatteryBars[1] = mStatusBar.findViewById(R.id.battery_bar_1);
         mSignalClusterEndPadding = getResources().getDimensionPixelSize(R.dimen.signal_cluster_battery_padding);
         mStatusIcons = mStatusBar.findViewById(R.id.statusIcons);
         int batteryStyle = Settings.System.getInt(getContext().getContentResolver(),
@@ -541,6 +546,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             animateHide(mRightClock, animate, true);
         }
         animateHide(mSystemIconArea, animate, true);
+        for (View batteryBar: mBatteryBars) {
+            animateHide(batteryBar, animate, false);
+        }
     }
 
     private void showSystemIconArea(boolean animate) {
@@ -552,6 +560,13 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                 animateShow(mRightClock, animate);
             }
             animateShow(mSystemIconArea, animate);
+            if (mShowLogo == 2) {
+                animateShow(mXtendedLogoRight, animate);
+                updateLogoSettings(animate);
+            }
+            for (View batteryBar: mBatteryBars) {
+                 animateShow(batteryBar, animate);
+			}
         } else {
             // We are in the middle of a system status event animation, which will animate the
             // alpha (but not the visibility). Allow the view to become visible again
