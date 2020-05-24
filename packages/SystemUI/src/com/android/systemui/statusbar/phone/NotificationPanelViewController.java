2944e7ac0d9adb98db5a2d1065eb0c7027cd9d50
TikkiTikki <radstylemusic@gmail.com>
2020-05-23 23:36:01 -0300

    Add Ambient Customizations [1/2]
    This adds the option to add custom text and a custom image on the ambient screen. This option is still in development, so be patient, the bugs that arise will be resolved.
    Based on OmniROM Edge Lighting and Custom Headers.
    
    [SuperDroidBond: adapt for R + A13 & put it behind notification lights]
    
    Good Work by @TikkiX2
    
    Change-Id: I8bbda7e908ef4bae892c59fdbcf6032a799b3f5b
    
    fixup! Ambient Customizations
    
    SDB:
    1. Fix Text Animation
    2. Fix Pulse Lights not visible if Ambient Image activated
    3. Catch NPE in KeyguardVisibilityHelper
    4. Misc. fixes for Ambient Text & Images
    
    Change-Id: Icbc66effd362d5e7d4e77d977960778bfc19dc7a
    
    Co-authored-by: TikkiTikki <radstylemusic@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -223,6 +223,8 @@ import com.android.systemui.wallet.controller.QuickAccessWalletController;
 import com.android.wm.shell.animation.FlingAnimationUtils;
 
 import com.android.internal.util.xtended.XtendedUtils;
+import com.android.systemui.xtended.AmbientText;
+import com.android.systemui.xtended.AmbientCustomImage;
 
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
@@ -637,6 +639,10 @@ public class NotificationPanelViewController extends PanelViewController {
     private boolean mAmbientPulseLightRunning;
     private boolean mAmbientPulseRanOnce = false; // used only for repeats
     public static final String CANCEL_NOTIFICATION_PULSE_ACTION = "cancel_notification_pulse";
+
+    // Ambient Customization
+    private AmbientText mAmbientText;
+    private AmbientCustomImage mAmbientCustomImage;
 
     private boolean mAnimatingQS;
 
@@ -1067,6 +1073,8 @@ public class NotificationPanelViewController extends PanelViewController {
         mPreviewContainer = mView.findViewById(R.id.preview_container);
         mKeyguardBottomArea.setPreviewContainer(mPreviewContainer);
         mPulseLightsView = mView.findViewById(R.id.lights_container);
+        mAmbientText = (AmbientText) mView.findViewById(R.id.text_container);
+        mAmbientCustomImage = (AmbientCustomImage) mView.findViewById(R.id.image_container);
 
         mReTickerComeback = mView.findViewById(R.id.ticker_comeback);
         mReTickerComebackIcon = mView.findViewById(R.id.ticker_comeback_icon);
@@ -3908,6 +3916,58 @@ public class NotificationPanelViewController extends PanelViewController {
         if (mPulseLightsView != null) {
             updatePulseLightState(dozing);
         }
+        if (mAmbientText != null) {
+            updateAmbientTextState(dozing);
+        }
+        if (mAmbientCustomImage != null) {
+            updateAmbientCustomImageState(dozing);
+        }
+    }
+
+    private void updateAmbientTextState(boolean dozing) {
+        boolean mAmbientTextEnable = Settings.System.getIntForUser(
+                mView.getContext().getContentResolver(), Settings.System.AMBIENT_TEXT,
+                0, UserHandle.USER_CURRENT) != 0;
+        boolean ambientTextAnimated = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
+                Settings.System.AMBIENT_TEXT_ANIMATION, 0, UserHandle.USER_CURRENT) != 0;
+
+        if (mAmbientTextEnable) {
+            if (dozing) {
+                // TODO on screen off should we restart pulse?
+                // if that should work we need to decide at this point
+                // if the current notifications "would" turn the screen on
+                // just checking hasActiveClearableNotifications is obviusly not
+                // enough here - so for now dont even try to do it
+                mAmbientText.animateText(ambientTextAnimated);
+                mAmbientText.update();
+                mAmbientText.setVisibility(View.VISIBLE);
+            } else {
+                // screen on!
+                mAmbientText.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private void updateAmbientCustomImageState(boolean dozing) {
+        boolean mAmbientCustomImageEnable = Settings.System.getIntForUser(
+                mView.getContext().getContentResolver(), Settings.System.AMBIENT_IMAGE,
+                0, UserHandle.USER_CURRENT) != 0;
+
+        if (mAmbientCustomImageEnable) {
+            if (dozing) {
+                // TODO on screen off should we restart pulse?
+                // if that should work we need to decide at this point
+                // if the current notifications "would" turn the screen on
+                // just checking hasActiveClearableNotifications is obviusly not
+                // enough here - so for now dont even try to do it
+                mAmbientCustomImage.update();
+                mAmbientCustomImage.setVisibility(View.VISIBLE);
+            } else {
+                // screen on!
+                mAmbientCustomImage.setVisibility(View.GONE);
+                mAmbientCustomImage.update();
+            }
+        }
     }
 
     private void updatePulseLightState(boolean dozing) {
@@ -3958,6 +4018,13 @@ public class NotificationPanelViewController extends PanelViewController {
                 Settings.System.AMBIENT_LIGHT_PULSE_FOR_ALL, 0, UserHandle.USER_CURRENT) == 1;
         int repeats = Settings.System.getIntForUser(resolver,
                 Settings.System.NOTIFICATION_PULSE_REPEATS, 0, UserHandle.USER_CURRENT);
+        boolean ambientText = Settings.System.getIntForUser(resolver,
+                Settings.System.AMBIENT_TEXT, 0, UserHandle.USER_CURRENT) != 0;
+        boolean ambientTextAnimated = Settings.System.getIntForUser(resolver,
+                Settings.System.AMBIENT_TEXT_ANIMATION, 0, UserHandle.USER_CURRENT) != 0;
+        boolean ambientImage = Settings.System.getIntForUser(resolver,
+                Settings.System.AMBIENT_IMAGE, 0, UserHandle.USER_CURRENT) != 0;
+
         if (animatePulse) {
             mAnimateNextPositionUpdate = true;
         }
@@ -4033,6 +4100,42 @@ public class NotificationPanelViewController extends PanelViewController {
                     stopNotificationPulse();
                 }
             }
+        }
+        if (mAmbientText != null && ambientText) {
+           if (mPulsing) {
+               mAmbientText.animateText(ambientTextAnimated);
+               mAmbientText.update();
+               mAmbientText.setVisibility(View.VISIBLE);
+           } else {
+              if (mDozing) {
+                  mAmbientText.animateText(ambientTextAnimated);
+                  mAmbientText.update();
+                  mAmbientText.setVisibility(View.VISIBLE);
+              } else {
+                  mAmbientText.update();
+                  mAmbientText.setVisibility(View.GONE);
+              }
+           }
+        } else {
+            mAmbientText.update();
+            mAmbientText.setVisibility(View.GONE);
+        }
+        if (mAmbientCustomImage != null && ambientImage) {
+            if (mPulsing) {
+                mAmbientCustomImage.setVisibility(View.VISIBLE);
+                mAmbientCustomImage.update();
+            } else {
+                if (mDozing) {
+                    mAmbientCustomImage.update();
+                    mAmbientCustomImage.setVisibility(View.VISIBLE);
+                } else {
+                    mAmbientCustomImage.update();
+                    mAmbientCustomImage.setVisibility(View.GONE);
+                }
+            }
+        } else {
+            mAmbientCustomImage.setVisibility(View.GONE);
+            mAmbientCustomImage.update();
         }
         mNotificationStackScrollLayoutController.setPulsing(pulsing, animatePulse);
     }
