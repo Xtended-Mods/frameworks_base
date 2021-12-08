8f6e981ec7bde4e549e55c77a01c9a86530373ff
cjh1249131356 <cjh1249131356@gmail.com>
2021-12-08 09:34:01 +0800

    SystemUI: Improve gesture bar length [1/2]
    * Merge hidden into length settings.
    * Cleanup useless configs in overlay.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5913,13 +5913,6 @@ public final class Settings {
         public static final String RECENTS_LOCKED_TASKS = "recents_locked_tasks";
 
         /**
-         * Whether to hide navbar pill and keyboard space.
-         * Default 0.
-         * @hide
-         */
-        public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -11252,7 +11245,7 @@ public final class Settings {
 
         /**
          * Gesture navbar length mode.
-         * Supported modes: 0 for normal length, 1 for medium and 2 for long.
+         * Supported modes: 0 for hidden, 1 for normal length, 2 for medium and 3 for long.
          * Default 0.
          * @hide
          */
