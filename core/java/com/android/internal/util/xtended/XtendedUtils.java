48d85749679145952fa5c2940b5023b6e7e9462e
bigrushdog <randall.rushing@gmail.com>
2022-01-28 17:43:38 +0100

    Gestures nav: Add Skip/Previous Song Function abcdu: use our statusbar media actions implementation to have also remote spotify support, and to be able to send different media actions from any class with the proper keycode
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ie7cd23c368a02a530cd1fa71076251bb3918f3cb
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/com/android/internal/util/xtended/XtendedUtils.java
+++ core/java/com/android/internal/util/xtended/XtendedUtils.java
@@ -318,4 +318,15 @@ public class XtendedUtils {
                 break;
         }
     }
+
+    public static void sendSystemKeyToStatusBar(int keyCode) {
+       IStatusBarService service = getStatusBarService();
+       if (service != null) {
+           try {
+                service.handleSystemKey(keyCode);
+           } catch (RemoteException e) {
+               // do nothing.
+           }
+       }
+    }
 }
