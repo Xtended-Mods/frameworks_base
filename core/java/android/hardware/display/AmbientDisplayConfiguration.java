omnirom/android_frameworks_base_old

base: cleanup notification pulse values
a few unneded and a few with wrong names

Change-Id: I755282a0a7d7d7d1f556d10ac8fbb8af05447909

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -236,9 +236,9 @@ public class AmbientDisplayConfiguration {
 
     /** {@hide} */
     public boolean alwaysOnAmbientLightEnabled(int user) {
-        final boolean ambientLightsEnabled = boolSettingSystem(Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ENABLED, user, 0);
+        final boolean ambientLightsEnabled = boolSettingSystem(Settings.System.OMNI_AOD_NOTIFICATION_PULSE, user, 0);
         if (ambientLightsEnabled) {
-            boolean ambientLightsActivated = boolSettingSystem(Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, user, 0);
+            boolean ambientLightsActivated = boolSettingSystem(Settings.System.OMNI_AOD_NOTIFICATION_PULSE_ACTIVATED, user, 0);
             return ambientLightsActivated && !accessibilityInversionEnabled(user) && alwaysOnAvailable();
         }
         return false;

Xtended-Mods/frameworks_base

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -350,9 +350,9 @@ public class AmbientDisplayConfiguration {
 
     /** {@hide} */
     public boolean alwaysOnAmbientLightEnabled(int user) {
-        final boolean ambientLightsEnabled = boolSettingSystem(Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED, user, 0);
+        final boolean ambientLightsEnabled = boolSettingSystem(Settings.System.AOD_NOTIFICATION_PULSE, user, 0);
         if (ambientLightsEnabled) {
-            boolean ambientLightsActivated = boolSettingSystem(Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, user, 0);
+            boolean ambientLightsActivated = boolSettingSystem(Settings.System.AOD_NOTIFICATION_PULSE_ACTIVATED, user, 0);
             return ambientLightsActivated && !accessibilityInversionEnabled(user) && alwaysOnAvailable();
         }
         return false;