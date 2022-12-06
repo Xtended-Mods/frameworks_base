pixeldust-project-caf/android_frameworks_base

Gestures nav: Add Partial Screenshot action. * Switch to the method in our utils, as this also supports the partial screenshot
Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: Ie8045e35ecb19e4c07113865c6a092859aab9746

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1012,7 +1012,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 8: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                PixeldustUtils.takeScreenshot(true);
                 break;
             case 9: // Notification panel
                 ActionUtils.toggleNotifications();
@@ -1037,6 +1037,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 break;
             case 16: // Powermenu
                 PixeldustUtils.showPowerMenu();
+                break;
+            case 17: // Partial Screenshot
+                PixeldustUtils.takeScreenshot(false);
                 break;
         }
     }

Xtended-Mods/frameworks_base

Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1001,7 +1001,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 8: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                XtendedUtils.takeScreenshot(true);
                 break;
             case 9: // Notification panel
                 ActionUtils.toggleNotifications();
@@ -1026,6 +1026,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 break;
             case 16: // Powermenu
                 XtendedUtils.showPowerMenu();
+                break;
+            case 17: // Partial Screenshot
+                XtendedUtils.takeScreenshot(false);
                 break;
         }
     }